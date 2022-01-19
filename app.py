7127e211eeba729c042867983d0487dd34e3e03d
Jose-Puente2001 <josegabrielpuente75@gmail.com>
2022-01-19 09:11:35 -0400

    mostrado de datos en la tabla

--- app.py
+++ app.py
@@ -6,12 +6,14 @@ import sqlite3
 
 class Patient:
 
+    db_name = 'database.db'
+
     def __init__(self, window):
         self.wind = window
         self.wind.title('Registro de Pacientes')
 
         frame = LabelFrame(self.wind, text="Registre nuevo Paciente")
-        frame.grid(row = 0, column = 0, columnspan = 3, pady = 20)
+        frame.grid(row = 0, column = 0, columnspan = 5, pady = 20)
 
         # Name Input
         Label(frame, text='Nombre: ').grid(row = 1, column = 0)
@@ -39,11 +41,34 @@ class Patient:
         
         #table
         self.tree = ttk.Treeview(height = 15, columns=[f"#{n}" for n in range(0, 3)])
-        self.tree.grid(row = 6, column = 0, columnspan = 2)
+        self.tree.grid(row = 6, column = 0, columnspan = 4)
         self.tree.heading('#0', text = 'Nombre', anchor = CENTER)
         self.tree.heading('#1', text = 'Apellido', anchor = CENTER)
         self.tree.heading('#2', text = 'Area', anchor = CENTER)
         self.tree.heading('#3', text = 'Precio' , anchor= CENTER)
+
+        self.get_patients()
+
+
+    def run_query(self, query, parameters = ()):
+        with sqlite3.connect(self.db_name) as conn:
+             cursor = conn.cursor()
+             result = cursor.execute(query, parameters)
+             conn.commit()
+        return result
+
+    def get_patients(self):
+
+       records = self.tree.get_children()
+       for element in records:
+         self.tree.delete(element)
+
+       query =  'SELECT * FROM patient ORDER BY name DESC'
+       db_rows = self.run_query(query)
+       for row in db_rows:
+            self.tree.insert('', 0, text=row[1], values = (row[2], row[3], row[4]))
+            
+
 
 if __name__ == '__main__':
     window = Tk()
